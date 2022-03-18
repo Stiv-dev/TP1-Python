8821061368e3c633b5fee1d319334013b13b2521
YAGAPIN <yagapin@et.esiea.fr>
2022-03-18 16:48:59 +0100

    Premier commit

---
+++ Syracuse.py
@@ -0,0 +1,10 @@
+def syracuse(U):
+    while U != 1:
+        if U%2 == 0 :
+            U = U // 2
+        else :
+            U = U*3 + 1
+        print(U)
+U = int(input("Veuillez entrer le premier terme :"))
+print(U)
+syracuse(U)
